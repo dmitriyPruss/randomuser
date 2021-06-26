3cd3358928f0cb7f06f112255033cc4f0e0aea50
Viktoriia <pecourses@gmail.com>
2021-06-26 18:48:45 +0300

    add  createUserListItem, createUserImage

--- js/index.js
+++ js/index.js
@@ -13,5 +13,29 @@ fetch(
   .then(({ results }) => renderUsers(results));
 
 function renderUsers(users) {
-  console.dir(users);
+  const liUserCollection = users.map(user => createUserListItem(user));
+  const userList = document.getElementById('userList');
+  userList.append(...liUserCollection);
 }
+
+function createUserListItem({
+  name: { first: firstName, last: lastName },
+  picture: { large: userImageSrc },
+}) {
+  const userListItem = document.createElement('li');
+  userListItem.classList.add('userListItem');
+
+  userListItem.append(createUserImage(userImageSrc));
+  userListItem.append(createUserFullName());
+
+  return userListItem;
+}
+
+function createUserImage(userImageSrc) {
+  const img = new Image();
+  img.src = userImageSrc;
+  img.alt = 'user profile image';
+  return img;
+}
+// Написать createUserFullName
+function createUserFullName() {}
